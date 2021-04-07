e4fe7eab87ca0f1eadc6944ad090cc58457bb30f
Shorn <bitbucket-shorn@snkmail.com>
2021-04-07 11:12:18 +1000

    remove logic to specify post parameters to avoid exceptions
    it can't work in the long run, so don't confuse the issue

--- src/test/java/turnip/functional/endpoint/AuthzEndpointTest.java
+++ src/test/java/turnip/functional/endpoint/AuthzEndpointTest.java
@@ -1,9 +1,7 @@
 package turnip.functional.endpoint;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
-import org.junit.jupiter.api.TestInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
@@ -13,12 +11,9 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
-import turnip.endpoint.MiscAdmin;
 import turnip.functional.FunctionalTest;
 import turnip.functional.spring.bean.UserManager;
-import turnip.service.AuthzSvc;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Stream;
 
@@ -28,40 +23,22 @@ import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 import static org.springframework.http.HttpMethod.POST;
 import static turnip.util.RestUtil.createEntityWithBearer;
 
+/**
+This test will cause excpetions to show p in the log - they can be ignored.
+It's too expensive to engineer a generic solution to generate correct 
+inputs to every endpoint in order to avoid the exceptions.  So you'll see 
+the tests generate lots on NullPointerExceptions and other input-related errors.
+*/
 public class AuthzEndpointTest extends FunctionalTest {
   public static final Map<String, String> EMPTY_POST_PARAM = Map.of(
     "requestData", "from AuthzEndpointTest");
 
-  /** 
-   The value is an object that will be passed to the POST request.
-   Should be defined as a value that will allow the endpoint request to succeed 
-   without throwing an internal error because of NullPonterException or 
-   similar.  
-   The intent is to minimise polluting the test logs with a bunch of 
-   error stack traces that aren't actually a problem (note the tests still  
-   still pass if the endpoint fails).  The errors are just distracting and 
-   they waste the time of everybody so does the right thing and investigates 
-   the errors thinking they're a potential problem.
-   */
-  private Map<String, Object> postParams;
-  
   @Autowired private UserManager userManager;
   
   @TestFactory
   public Stream<DynamicTest> allEndpointsShouldBeAuthorized() {
     return jettyTestServer.getTurnipApiHandlerMethods().entrySet().stream().
       flatMap((i) -> createEndpointTests(i.getKey(), i.getValue()));
-  }
-  
-  @BeforeEach
-  public void configureAllowedParams(TestInfo testInfo){
-    if( postParams != null ){
-      return;
-    }
-    postParams = new HashMap<>();
-    postParams.put("/api/add-user", new MiscAdmin.AddUserRequest(
-      userManager.formatNewUserEmail(getTestPrefix(testInfo)),
-      AuthzSvc.USER_ROLE ));
   }
   
   public Stream<DynamicTest> createEndpointTests(
@@ -71,14 +48,12 @@ public class AuthzEndpointTest extends FunctionalTest {
     String path = mapping.getDirectPaths().stream().toList().get(0);
     return mapping.getMethodsCondition().getMethods().stream().map(iMethod-> {
       if( iMethod == RequestMethod.GET ){
-        return dynamicTest(
-          "GET " + path,
+        return dynamicTest("GET " + path,
           () -> testGetEndpoint(path, handler));
       }
       
       if( iMethod == RequestMethod.POST ){
-        return dynamicTest(
-          "POST " + path,
+        return dynamicTest("POST " + path,
           () -> testPostEndpoint(path, handler));
       }
 
@@ -87,8 +62,9 @@ public class AuthzEndpointTest extends FunctionalTest {
     });
   }
 
-  /* This likely won't work very well for dynamic path structures, but I 
-  mostly only use POST requests anyway. */
+  /** This basic implementation won't work very well for dynamic path
+   structures, but I mostly only use POST requests anyway.
+   */
   private void testGetEndpoint(String path, HandlerMethod handler) {
     HttpEntity<Object> entity =
       createEntityWithBearer(token.getNonUser());
@@ -120,9 +96,8 @@ public class AuthzEndpointTest extends FunctionalTest {
   }
 
   private void testPostEndpoint(String path, HandlerMethod handler) {
-    Object body = postParams.getOrDefault(path, EMPTY_POST_PARAM);
     HttpEntity<Object> entity =
-      createEntityWithBearer(token.getNonUser(), body);
+      createEntityWithBearer(token.getNonUser(), EMPTY_POST_PARAM);
 
     try {
       rest.exchange(turnipApiServerUrl(path), POST, entity, String.class);
@@ -138,7 +113,7 @@ public class AuthzEndpointTest extends FunctionalTest {
     }
 
     // any POST endpoint should be callable by the admin user
-    entity = createEntityWithBearer(token.getAdmin(), body);
+    entity = createEntityWithBearer(token.getAdmin(), EMPTY_POST_PARAM);
     try {
       rest.exchange(turnipApiServerUrl(path), POST, entity, String.class);
       // if the test gets to here, then the server got past the security 
